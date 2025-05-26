e97651c81bb60c3afb805626bbf709f3f2101634
ASionville <aubinsionville@gmail.com>
2025-05-26 22:17:00 +0200

    Improved default config

--- config.m
+++ config.m
@@ -1,12 +1,12 @@
 % Configuration des paramètres pour Ant Colony Optimization (gridsearch)
-cfg.nAnts       = [1000];
+cfg.nAnts       = [100 1000];
 cfg.maxIterations = [100];
-cfg.alpha       = [2];
-cfg.beta        = [2];
-cfg.rho         = [0.5];
+cfg.alpha       = [1 2];
+cfg.beta        = [1 2];
+cfg.rho         = [0.1 0.5];
 cfg.Q           = [1];
-cfg.nCities     = [100];
+cfg.nCities     = [10 100 1000];
 cfg.seed        = [42]; % Pour reproductibilité
-cfg.visualization = 'all'; % 'all', 'best', 'none'
+cfg.visualization = 'best'; % 'all', 'best', 'none'
 cfg.minImprovement = [1e-6]; % Amélioration minimale considérée
 cfg.patience = [20]; % Nombre d'itérations sans amélioration avant arrêt anticipé
